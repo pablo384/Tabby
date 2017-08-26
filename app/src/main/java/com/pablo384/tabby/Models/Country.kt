ee232de621010c0e48c26c81eee683a49d987230
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-26 15:55:11 -0400

    POJO Creado

---
+++ app/src/main/java/com/pablo384/tabby/Models/Country.kt
@@ -0,0 +1,6 @@
+package com.pablo384.tabby.Models
+
+/**
+ * Created by pablo384 on 26/08/17.
+ */
+class Country (val name:String, val countryCode:String) {}
---
+++ app/src/main/java/com/pablo384/tabby/Models/Person.kt
@@ -0,0 +1,6 @@
+package com.pablo384.tabby.Models
+
+/**
+ * Created by pablo384 on 26/08/17.
+ */
+class Person(val name:String, val country: Country) {}
