90517b1f92345df34445c06f11de46ba2fb84dba
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-27 14:56:18 -0400

    FormFragment listo, falta comunicacion entre fragments

--- app/src/main/java/com/pablo384/tabby/Fragments/FormFragment.kt
+++ app/src/main/java/com/pablo384/tabby/Fragments/FormFragment.kt
@@ -6,8 +6,15 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.pablo384.tabby.Models.Country
+import com.pablo384.tabby.Models.Person
 
 import com.pablo384.tabby.R
+import kotlinx.android.synthetic.main.fragment_form.*
+import kotlinx.android.synthetic.main.fragment_form.view.*
+import android.R.array
+import android.widget.Toast
 
 
 /**
@@ -19,6 +26,16 @@ class FormFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view= inflater!!.inflate(R.layout.fragment_form, container, false)
+        val lista= resources.getStringArray(R.array.country_array)
+        val adapter:ArrayAdapter<String> = ArrayAdapter(this.context,
+                android.R.layout.simple_spinner_item, lista)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        view.spinnerCountry.adapter=adapter
+
+        view.buttonAdd.setOnClickListener { Toast.makeText(this.context,"Nombre: ${editTextName.text} y pais ${spinnerCountry.selectedItem.toString()}", Toast.LENGTH_SHORT).show() }
+
+
 
         // Inflate the layout for this fragment
         return view
--- app/src/main/java/com/pablo384/tabby/Models/Country.kt
+++ app/src/main/java/com/pablo384/tabby/Models/Country.kt
@@ -3,6 +3,22 @@ package com.pablo384.tabby.Models
 /**
  * Created by pablo384 on 26/08/17.
  */
-class Country (val name:String, val countryCode:String) {
-    fun getURLCountry()= "http://www.geognos.com/api/en/countries/flag/$countryCode.png"
+class Country (val name:String) {
+    fun getURLCountry()= "http://www.geognos.com/api/en/countries/flag/${getCountryCode(name)}.png"
+    fun getCountryCode(country:String):String{
+        when(country){
+            "Republica Dominicana" -> return "DO"
+            "España" -> return "ES"
+            "Argentina" -> return "AR"
+            "Bolivia"-> return "BO"
+            "Chile"->return "CL"
+            "Colombia"-> return "CO"
+            "Ecuador"->return "EC"
+            "Mexico"-> return "MX"
+            "Perú"->return "PE"
+            "Uruguay"-> return "UY"
+            "Venezuela"-> return "VE"
+            else -> return "DO"
+        }
+    }
 }
